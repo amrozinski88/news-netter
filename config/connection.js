7295458f0ec139f245a73430693966ff4ddd8bed
AdamM <ajmrozinski@gmail.com>
2019-11-05 00:33:25 -0500

    added mongo connection on connection js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URI ||"mongodb://localhost/articles_db"
 
-
-module.exports = mongoose.connect("mongodb://localhost/articles_db",{
+module.exports = mongoose.connect(MONGODB_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
